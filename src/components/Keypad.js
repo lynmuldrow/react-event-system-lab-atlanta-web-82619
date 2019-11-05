94c7f3f945e494c8f5800751420311963a5b5030
Lyn Muldrow <lyn.muldrow@flatironschool.com>
2019-11-05 19:28:18 +0000

    Automatically backed up by Learn

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,10 @@
 // Code Keypad Component Here
+export default class Keypad extends React.Component {
+  
+  render () {
+    return (
+      <input type="password" onKeyUp={this.handleKeyUp}>
+    );
+  }
+  
+}
